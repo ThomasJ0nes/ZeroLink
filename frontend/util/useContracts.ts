4c5972b9fd0fb035d085e725ce261d2f64f7ae8d
Tom Jones <94535525+ThomasJ0nes@users.noreply.github.com>
2024-09-01 13:52:56 +0100

    Create Subscription page, Added Footer, Added Logo

--- frontend/util/useContracts.ts
+++ frontend/util/useContracts.ts
@@ -1,8 +1,10 @@
+// hook to use contracts
 import { useWeb3Auth } from "@/context/Web3AuthContext";
 import { Contract, ethers } from "ethers";
 import SubscriptionManagerABI from "../../contracts/ignition/deployments/chain-11155111/artifacts/SubscriptionManagerModule#SubscriptionManager.json";
 import PaymentProcessorABI from "../../contracts/ignition/deployments/chain-84532/artifacts/PaymentProcessorModule#PaymentProcessor.json";
 
+// contract addresses
 const SUBSCRIPTION_MANAGER_ADDRESS = '0x6AA2F7F0Ea2aF5f3CF25E2026D054dfdF32aAd8a';
 const PAYMENT_PROCESSOR_ADDRESS = '0xB4b29499D53839Fd601725A6B4c2abbdF05A7133';
 
